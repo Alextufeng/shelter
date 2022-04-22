2bb846276117eb8d9dcf3d6d02c19c3e3e2494a4
Alextufeng <salman-aka@mail.ru>
2022-04-22 17:55:41 +0300

    feat: add toggle menu js pets

--- pages/pets/index.js
+++ pages/pets/index.js
@@ -1,3 +1,41 @@
+const body = document.querySelector('body')
+const burger = document.querySelector('.burger_icon');
+const menu = document.querySelector('.nav_list');
+
+const toggleMenu = function(){
+	menu.classList.toggle('active');
+    burger.classList.toggle('active');
+    body.classList.toggle('active');
+};
+
+/*
+const menuLink = document.getElementsByClassName('.nav_item');
+for (let i=0 ; i<menuLink.length; i++) {
+  menuLink[i].addEventListener("click", function(){
+    toggleMenu();
+  });
+}*/
+
+burger.addEventListener('click', function(e) {
+    e.stopPropagation();
+    toggleMenu();
+});
+
+document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == menu || menu.contains(target);
+    const its_btnMenu = target == burger;
+    const menu_is_active = menu.classList.contains('active');
+    
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+        toggleMenu();
+    }
+});
+
+
+
+
+/*
 [
     {
       "name": "Jennifer",
@@ -88,4 +126,4 @@
       "parasites": ["lice", "fleas"]
     }
   ]
-  
+  */
